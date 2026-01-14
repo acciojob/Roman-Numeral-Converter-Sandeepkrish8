cf90c0df44bf2e8d20442aa346e492964f258150
SANDEEP KRISH <168737689+Sandeepkrish8@users.noreply.github.com>
2026-01-14 12:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 function convertToRoman(num) {
+  num = Number(num); // 🔥 IMPORTANT FIX (string → number)
+
+  if (num === 0) return "";
+
   const symbols = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
   ];
 
   let result = "";
 
   for (let i = 0; i < symbols.length; i++) {
-    const [roman, value] = symbols[i];
-
-    while (num >= value) {
-      result += roman;
-      num -= value;
+    while (num >= symbols[i][1]) {
+      result += symbols[i][0];
+      num -= symbols[i][1];
     }
   }
 
