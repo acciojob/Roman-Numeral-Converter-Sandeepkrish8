0565274ca97a1ccdfeccb84927cfc3b691711aae
SANDEEP KRISH <168737689+Sandeepkrish8@users.noreply.github.com>
2026-01-14 12:22:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 function convertToRoman(num) {
-    // Roman numeral symbols including subtractive ones
-    const romanMap = [
-        ["M", 1000],
-        ["CM", 900],
-        ["D", 500],
-        ["CD", 400],
-        ["C", 100],
-        ["XC", 90],
-        ["L", 50],
-        ["XL", 40],
-        ["X", 10],
-        ["IX", 9],
-        ["V", 5],
-        ["IV", 4],
-        ["I", 1]
-    ];
+  const symbols = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-    let result = "";
+  let result = "";
 
-    for (let [symbol, value] of romanMap) {
-        // Append symbol while the number is large enough
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
+  for (let i = 0; i < symbols.length; i++) {
+    const [roman, value] = symbols[i];
+
+    while (num >= value) {
+      result += roman;
+      num -= value;
     }
+  }
 
-    return result;
+  return result;
 }
+
 
 // DO NOT change anything below
 function main() {
